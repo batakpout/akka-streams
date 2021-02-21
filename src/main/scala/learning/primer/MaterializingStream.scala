8cdbd920654776ae655a8b4dc93217a38011f66c
aamirfayaz <dropmeataamir@gmail.com>
2021-02-21 16:55:25 +0530

    some examples on materialization

--- src/main/scala/learning/primer/MaterializingStream.scala
+++ src/main/scala/learning/primer/MaterializingStream.scala
@@ -11,6 +11,49 @@ import scala.util.{Failure, Random, Success}
 /**
   * Fetching a meaningful value out of a running stream
   */
+object Just_For_Understanding extends App {
+
+   implicit val system = ActorSystem("FirstPrinciples-10")
+   implicit val materializer = ActorMaterializer()
+
+   val r = new Random(1)
+   val source: Source[Int, NotUsed] = Source(1 to 100)
+   val flow: Flow[Int, Float, NotUsed] = Flow[Int].map[Float](_ => r.nextFloat())
+
+   /**
+     *  definition of Source and Source.via which returns a Flow back
+     *  final class Source[+Out, +Mat] // Out is what comes out of Stream and Mat is its Materialized value
+     *  override def Source.via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): Repr[T] = viaMat(flow)(Keep.left)
+     *  override type Repr[+O] = Source[O, Mat @uncheckedVariance]
+     */
+   val sourceWithFlow: Source[Float, NotUsed] = source.via[Float, NotUsed](flow) // this is keep.left
+   /** Source.viaMat
+     override def viaMat[T, Mat2, Mat3](flow: Graph[FlowShape[Out, T], Mat2])(combine: (Mat, Mat2) ⇒ Mat3): Source[T, Mat3]
+     */
+   val graph4: Source[Float, NotUsed] = source.viaMat[Float, NotUsed, NotUsed](flow)(Keep.left)
+
+
+
+   val reduceSink: Sink[Float, Future[String]] = Sink.fold[String, Float](" ")((res, cur) => cur + res)
+   val reduceSin1k: Sink[Float, Future[Float]] = Sink.reduce[Float](_ + _)
+
+   /**
+     * Definition of Sink flow and Flow.to which returns a sink back
+     * final class Flow[-In, +Out, +Mat](
+     * def to[Mat2](sink: Graph[SinkShape[Out], Mat2]): Sink[In, Mat] = toMat(sink)(Keep.left)
+     * def toMat[Mat2, Mat3](sink: Graph[SinkShape[Out], Mat2])(combine: (Mat, Mat2) ⇒ Mat3): Sink[In, Mat3] =
+     *  Mat2 means what is materialized value of to(Sink), and Mat3 means what will be the final materialized out of this sink
+     * */
+   val flowWithSink: Sink[Int, NotUsed] = flow.to[Future[String]](reduceSink) // coz to is Keep.left by default
+   val flowWithSink2: Sink[Int, NotUsed] = flow.toMat[Future[String], NotUsed](reduceSink)(Keep.left)
+   val flowWithSink1: Sink[Int, Future[String]] =  flow.toMat[Future[String], Future[String]](reduceSink)((flowOutput, sinkOutput ) => sinkOutput)
+
+   val graph: RunnableGraph[NotUsed] = source.via(flow).to(reduceSink)
+   val graph2: RunnableGraph[Future[String]] = source.via(flow).toMat(reduceSink)(Keep.right)
+   val graph3: RunnableGraph[Future[String]] = source.viaMat(flow)(Keep.right).toMat(reduceSink)(Keep.right)
+
+   //val rG: RunnableGraph[NotUsed] = sourceWithFlow.to(reduceSink)
+}
 
 object MaterializingStream_1 extends App {
 
@@ -66,6 +109,7 @@ object MaterializingStream_2 extends App {
    Thread.sleep(2000)
 }
 
+
 object MaterializingStream_3 extends App {
 
    implicit val system = ActorSystem("FirstPrinciples-10")
@@ -75,29 +119,26 @@ object MaterializingStream_3 extends App {
    val source: Source[Int, NotUsed] = Source(1 to 100)
    val flow: Flow[Int, Float, NotUsed] = Flow[Int].map[Float](_ => r.nextFloat())
 
-   /**
-     *  definition of Source and Source.via which returns a Flow back
-     *  final class Source[+Out, +Mat] // Out is what comes out of Stream and Mat is its Materialized value
-     *  override def Source.via[T, Mat2](flow: Graph[FlowShape[Out, T], Mat2]): Repr[T] = viaMat(flow)(Keep.left)
-     *  override type Repr[+O] = Source[O, Mat @uncheckedVariance]
-     */
-   val sourceWithFlow: Source[Float, NotUsed] = source.via[Float, NotUsed](flow)
+   val sourceWithFlow: Source[Float, NotUsed] = source.via(flow) // this is keep.left
+
+   val graph4: Source[Float, NotUsed] = source.viaMat(flow)(Keep.right) // coz return type of Flow is NotUsed
+
+
 
    val reduceSink: Sink[Float, Future[String]] = Sink.fold[String, Float](" ")((res, cur) => cur + res)
    val reduceSin1k: Sink[Float, Future[Float]] = Sink.reduce[Float](_ + _)
 
-   /**
-     * Definition of Sink flow and Flow.to which returns a sink back
-     * final class Flow[-In, +Out, +Mat](
-     * def to[Mat2](sink: Graph[SinkShape[Out], Mat2]): Sink[In, Mat] = toMat(sink)(Keep.left)
-     * def toMat[Mat2, Mat3](sink: Graph[SinkShape[Out], Mat2])(combine: (Mat, Mat2) ⇒ Mat3): Sink[In, Mat3] =
-     *  Mat2 means what is materialized value of to(Sink), and Mat3 means what will be the final materialized out of this sink
-     * */
-   val flowWithSink: Sink[Int, NotUsed] = flow.to[Future[String]](reduceSink) // coz to is Keep.left by default
-   val flowWithSink2: Sink[Int, NotUsed] = flow.toMat[Future[String], NotUsed](reduceSink)(Keep.left)
-   val flowWithSink1: Sink[Int, Future[String]] =  flow.toMat[Future[String], Future[String]](reduceSink)((flowOutput, sinkOutput ) => sinkOutput)
 
-   //val rG: RunnableGraph[NotUsed] = sourceWithFlow.to(reduceSink)
+   val flowWithSink: Sink[Int, NotUsed] = flow.to(reduceSink) // coz to is Keep.left by default
+   val flowWithSink2: Sink[Int, NotUsed] = flow.toMat(reduceSink)(Keep.left)
+   val flowWithSink1: Sink[Int, Future[String]] =  flow.toMat(reduceSink)((flowOutput, sinkOutput ) => sinkOutput)
+   val flowWithSink3: Sink[Int, Future[String]] =  flow.toMat(reduceSink)(Keep.right)
+
+   val graph: RunnableGraph[NotUsed] = source.via(flow).to(reduceSink)
+
+   val graph2: RunnableGraph[Future[String]] = source.via(flow).toMat(reduceSink)(Keep.right)
+   val graph3: RunnableGraph[Future[String]] = source.viaMat(flow)(Keep.right).toMat(reduceSink)(Keep.right)
+
 }
 object MaterializingStream_432 extends App {
 
